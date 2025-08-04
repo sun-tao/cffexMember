b2eb51be187056cb0ab341f35fee2258bdf651d9
sun-tao <1355316300@qq.com>
2025-08-04 14:35:00 +0800

    first commit

---
+++ src/main/java/com/example/cffexmember/CffexmemberApplication.java
@@ -0,0 +1,13 @@
+package com.example.cffexmember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CffexmemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CffexmemberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cffexmember/CffexmemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cffexmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CffexmemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
