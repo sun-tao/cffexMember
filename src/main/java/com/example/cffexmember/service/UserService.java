4a6e5d90b4bf1f66fdaab17f9ed5de27f98a3867
sun-tao <1355316300@qq.com>
2025-08-06 14:40:58 +0800

    调整代码结构

--- src/main/java/com/example/cffexmember/service/UserService.java
+++ src/main/java/com/example/cffexmember/service/UserService.java
@@ -2,10 +2,15 @@ package com.example.cffexmember.service;
 
 import com.example.cffexmember.entity.User;
 import com.example.cffexmember.mapper.UserMapper;
+import com.example.cffexmember.model.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
@@ -54,19 +59,36 @@ public class UserService {
         return userMapper.deleteById(id);
     }
 
-}
-import com.example.cffexmember.model.Result;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-public interface UserService {
-
-    Result userLogin(String username, String password,
+    public Result userLogin(String username, String password,
                      HttpSession session,
                      HttpServletRequest request,
-                     HttpServletResponse response);
-
+                     HttpServletResponse response){
+        Result<User> result = new Result<>();
+        User user = userMapper.selectByUsername(username);
+        if (user == null) {
+            result.setCode(-1);
+            result.setMessage("用户名不存在");
+            return result;
+        } else if (!user.getPassword().equals(password)) {
+            result.setCode(-1);
+            result.setMessage("密码错误");
+            return result;
+        }
+        // 将登录用户信息保存到session中
+        session.setAttribute("userInfo", user);
+        // 保存cookie，实现自动登录
+        Cookie cookie = new Cookie("cookie", username);
+        // expires in 30 minutes
+        cookie.setMaxAge(30 * 60);
+        // 设置为当前项目下都携带这个cookie
+        cookie.setPath(request.getContextPath());
+        // 向客户端发送cookie
+        response.addCookie(cookie);
+        result.setCode(0);
+        result.setMessage("登录成功");
+        result.setData(user);
+        return result;
+    }
 
 }
+
